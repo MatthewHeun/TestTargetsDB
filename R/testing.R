7b41a4ce3a7fc832d5a9fa1869a4a545a48d7fc7
Matthew Heun <matthew.heun@me.com>
2024-01-05 15:58:13 -0500

    Add test file

---
+++ R/testing.R
@@ -0,0 +1,40 @@
+# Examples from https://www.datacareer.de/blog/connect-to-postgresql-with-r-a-step-by-step-example/
+
+conn <- dbConnect(RPostgres::Postgres(), 
+                  dbname = "postgres", 
+                  host = "localhost", 
+                  port = 5432, 
+                  user = "postgres")
+dbListTables(conn) 
+dbDisconnect(conn)
+
+
+
+conn <- dbConnect(RPostgres::Postgres(), 
+                  dbname = "mkh2", 
+                  host = "localhost", 
+                  port = 5432, 
+                  user = "mkh2")
+dbListTables(conn) 
+dbDisconnect(conn)
+
+
+
+
+conn <- dbConnect(drv = RPostgres::Postgres(), 
+                  dbname = "postgres", 
+                  host = "153.106.113.125", 
+                  port = 5432, 
+                  user = "postgres")
+dbListTables(conn) 
+dbDisconnect(conn)
+
+conn <- dbConnect(drv = RPostgres::Postgres(), 
+                  dbname = "mkh2", 
+                  host = "153.106.113.125", 
+                  port = 5432, 
+                  user = "mkh2")
+dbListTables(conn) 
+dbDisconnect(conn)
+
+
